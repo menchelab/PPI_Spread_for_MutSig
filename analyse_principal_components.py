59cf943c003ef983bc722a404c250050386b59e0
Joel Hancock <joel@menchelab.com>
2023-07-26 17:23:05 +0200

    correct clustermap

--- analyse_principal_components.py
+++ analyse_principal_components.py
@@ -7,22 +7,22 @@ matplotlib.use("MacOSX")
 import matplotlib.pyplot as plt
 
 parameter = sys.argv[1]
-pcs = pd.read_csv("data/pca_components_"+parameter+".csv", index_col=0)
+pcs = pd.read_csv("data/pca_components_"+parameter+".csv", index_col=0).T
 
 layout = pd.read_csv("data/ppi_layout.csv", index_col=0)
 ppi = nx.read_gml("data/ppi.gml")
 
 import seaborn as sns
 
-
-aaa = sns.clustermap(pcs.T, cmap="seismic", metric = 'cosine', col_cluster=False, center=0, vmax = 0.1, vmin=-0.1)
-aaa.savefig("figures/smoothed_"+parameter+"/pc_clustermap,png")
+aaa = sns.clustermap(pcs, cmap="seismic", metric = 'cosine', col_cluster=False, center=0, vmax = 0.1, vmin=-0.1)
+aaa.savefig("figures/smoothed_"+parameter+"/pc_clustermap.png", dpi=500)
+aaa.savefig("figures/smoothed_"+parameter+"/pc_clustermap.svg")
 aaa.fig.show()
 
 for pc in pcs.columns[:10]:
     fig,ax = plt.subplots(figsize=[10,10])
     nx.draw_networkx_nodes(ppi, ax=ax, pos = {i:v for i,v in layout.iterrows()}, node_color=pcs[pc].as_dict())
     nx.draw_networkx_edges(ppi, ax=ax, pos = {i:v for i,v in layout.iterrows()})
-    fig.savefig("figures/smoothed_"+parameter+"/pc.png", dpi=500)
-
+    fig.savefig("figures/smoothed_"+parameter+"/ppi_layout.png", dpi=500)
+    fig.savefig("figures/smoothed_"+parameter+"/ppi_layout.svg")
 
